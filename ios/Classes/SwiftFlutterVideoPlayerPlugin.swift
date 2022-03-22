2ad4e6945145f1fab72f12d244c14dadaf680717
manhnguyen3 <manh.nguyen3@tiki.vn>
2022-03-22 18:05:44 +0700

    update for ios

--- ios/Classes/SwiftFlutterVideoPlayerPlugin.swift
+++ ios/Classes/SwiftFlutterVideoPlayerPlugin.swift
@@ -3,12 +3,11 @@ import UIKit
 
 public class SwiftFlutterVideoPlayerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_video_player", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterVideoPlayerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+        /* register audio player */
+        AudioPlayer.register(with: registrar)
+
+        /* register video player */
+        VideoPlayerFactory.register(with: registrar)
   }
 }
